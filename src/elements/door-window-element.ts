14d7a2416c5dcdb62b4a4c3aa98a7e5521932af3
Ondrej Salplachta <ondrej@salplachta.net>
2025-01-03 21:22:07 +0100

    feat: Ability to define orientation of door / window [horizontal, vertical]. Default is horizontal feat: Default height of door / window is 7px -> no need to specify for each door / window

--- src/elements/door-window-element.ts
+++ src/elements/door-window-element.ts
@@ -8,28 +8,40 @@ import type { HassEntity } from "home-assistant-js-websocket";
 interface DoorWindowElementConfig extends ElementEntityBaseConfig {
     width: number;
     height: number;
+    orientation: "vertical" | "horizontal";
 }
 
 
 @customElement("door-window-element")
 export class DoorWindowElement extends ElementEntityArmableBase<DoorWindowElementConfig> {
-
+    setConfig(config: DoorWindowElementConfig) {
+        super.setConfig({
+            ...config,
+            height: config.height ?? 7,
+            orientation: config.orientation ?? "horizontal"
+        });
+    }
     protected renderEntityContent(entity: HassEntity) {
         if (!this._config || !this.hass)
             return nothing;
 
         const opened = entity.state == "on";
-        const svgPathArea = "M" + 0 + " " + 0 + " L" + 0 + " " + this._config.height + " L" + this._config.width + " " + this._config.height + " L" + this._config.width + " " + 0 + " Z";
+        const height = this._config.orientation == 'horizontal' ? this._config.height : this._config.width;
+        const width = this._config.orientation == 'horizontal' ? this._config.width : this._config.height;
+
+        const svgPathArea = "M" + 0 + " " + 0 + " L" + 0 + " " + height + " L" + width + " " + height + " L" + width + " " + 0 + " Z";
 
 
         const alarmState = this.getAlarmoSensorState();
         const color = opened ? 'blue' : alarmState ? (alarmState.armed ? 'red' : 'green') : 'white'
 
         return html`
-            <svg width="${this._config.width}px" height="${this._config.height}px">
-                <path d=${svgPathArea} fill=${color} stroke=${color} strokeDasharray=0 strokeWidth=1 />
-            </svg>
-            <last-change-text .entity=${entity}></last-change-text>
+            <div style="display:flex; align-items:center; gap:5px; flex-direction:${this._config.orientation == 'horizontal' ? 'column':'row'}">
+                <svg width="${width}px" height="${height}px">
+                    <path d=${svgPathArea} fill=${color} stroke=${color} strokeDasharray=0 strokeWidth=1 />
+                </svg>
+                <last-change-text .entity=${entity}></last-change-text>
+            </div>            
             `
     }
 }
