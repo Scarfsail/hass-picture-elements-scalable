b6a298d823fa31f9d558de2e7a6460434c31c858
Ondrej Salplachta <ondrej@salplachta.net>
2025-01-28 19:52:18 +0100

    feat: Added iframe-element

---
+++ src/elements/iframe-element.ts
@@ -0,0 +1,25 @@
+import { html, nothing } from "lit"
+import { customElement } from "lit/decorators.js";
+import { ElementBase, ElementBaseConfig } from "./base";
+import type { HassEntity } from "home-assistant-js-websocket";
+
+interface IframeElementConfig extends ElementBaseConfig {
+    height: number;
+    width:number;
+    src:string;
+}
+
+@customElement("iframe-element")
+export class IframeElement extends ElementBase<IframeElementConfig> {
+    
+    protected override renderContent() {
+        if (!this._config || !this.hass)
+            return nothing;
+
+        return html`
+            <iframe src=${this._config.src} style="width: ${this._config.width}px; height: ${this._config.height}px;">
+
+            </iframe>
+        `
+    }
+}
