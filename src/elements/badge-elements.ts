b64cb56e23c8ede970e47a650ebefa59916b44d4
Ondrej Salplachta <ondrej@salplachta.net>
2025-02-23 19:40:09 +0100

    feat: Added badge element and badge elements to show badges on the scalable view

---
+++ src/elements/badge-elements.ts
@@ -0,0 +1,38 @@
+import { html } from "lit"
+import { customElement } from "lit/decorators.js";
+import { ElementBase, ElementBaseConfig } from "./base";
+import { HassEntity } from "home-assistant-js-websocket";
+import { keyed } from 'lit/directives/keyed.js';
+
+interface BadgeElementsConfig extends ElementBaseConfig {
+    entities: string[];
+
+}
+interface Badge {
+    entity: string;
+    element: any;
+}
+@customElement("badge-elements")
+export class BadgeElements extends ElementBase<BadgeElementsConfig> {
+
+    private badges?: Badge[];
+
+    protected override renderContent() {
+        if (!this.badges) {
+            this.badges = this._config!.entities.map(entity => ({
+                "entity": entity,
+                "element": document.createElement('hui-entity-badge') as any
+            }))
+        }
+        for (const badge of this.badges) {
+            badge.element.setConfig({ entity: badge.entity });
+            badge.element.hass = this.hass;
+        }
+
+        return html`
+            <div style="display: flex; flex-wrap: wrap;gap: 5px">
+                ${this.badges.map(badge => keyed(badge.entity, html`${badge.element}`))}
+            </div>
+        `;
+    }
+}
