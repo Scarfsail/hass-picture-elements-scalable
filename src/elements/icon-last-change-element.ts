796d6023cd11069d25cea96c785bfc213dd6119c
Ondrej Salplachta <ondrej@salplachta.net>
2025-02-28 13:52:33 +0100

    fix: Fixed re-render of icon when the icon a picture and chnages

--- src/elements/icon-last-change-element.ts
+++ src/elements/icon-last-change-element.ts
@@ -12,14 +12,17 @@ interface IconLastChangeElementConfig extends ElementEntityBaseConfig {
 @customElement("icon-last-change-element")
 export class IconLastChangeElement extends ElementEntityBase<IconLastChangeElementConfig> {
     private element: any;    
+    private lastChange: string = "";
     protected override renderEntityContent(entity: HassEntity) {
 
-        if (!this.element)
+        if (!this.element || this.lastChange != entity.last_changed){
             this.element = document.createElement('hui-state-icon-element') as any;
+            this.lastChange = entity.last_changed;
+        }
 
         this.element.setConfig(this._config)
         this.element.hass = this.hass;
-
+        
         return html`
             ${this.element}
             <last-change-text .entity=${entity}></last-change-text>            
